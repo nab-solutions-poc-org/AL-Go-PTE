1368b9dc789bc08212aa810961427b9fa4d5c364
nab-solutions-poc-org <nab-solutions-poc-org@users.noreply.github.com>
2025-02-24 20:48:17 +0000

    Deploying AL-Go from main (6deb42e18649d97d06fd3f08d20d9c8497e9f4ce) to preview

--- .AL-Go/localDevEnv.ps1
+++ .AL-Go/localDevEnv.ps1
@@ -46,9 +46,9 @@ Write-Host -ForegroundColor Yellow @'
 
 $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString())"
 New-Item -Path $tmpFolder -ItemType Directory -Force | Out-Null
-$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/nab-solutions-poc-org/AL-Go-for-NAB/7c4fec6c2547eee4eb1db58a1d52076dcda88ce2/Actions/Github-Helper.psm1' -folder $tmpFolder
-$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/nab-solutions-poc-org/AL-Go-for-NAB/7c4fec6c2547eee4eb1db58a1d52076dcda88ce2/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
-DownloadHelperFile -url 'https://raw.githubusercontent.com/nab-solutions-poc-org/AL-Go-for-NAB/7c4fec6c2547eee4eb1db58a1d52076dcda88ce2/Actions/Packages.json' -folder $tmpFolder | Out-Null
+$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/nab-solutions-poc-org/AL-Go-for-NAB/6deb42e18649d97d06fd3f08d20d9c8497e9f4ce/Actions/Github-Helper.psm1' -folder $tmpFolder
+$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/nab-solutions-poc-org/AL-Go-for-NAB/6deb42e18649d97d06fd3f08d20d9c8497e9f4ce/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
+DownloadHelperFile -url 'https://raw.githubusercontent.com/nab-solutions-poc-org/AL-Go-for-NAB/6deb42e18649d97d06fd3f08d20d9c8497e9f4ce/Actions/Packages.json' -folder $tmpFolder | Out-Null
 
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
